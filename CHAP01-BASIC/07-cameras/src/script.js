20a2856441024cfcd995084a8d8896406bdb862a
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-02-16 21:35:59 +0100

    OrbitCamera

--- CHAP01-BASIC/07-cameras/src/script.js
+++ CHAP01-BASIC/07-cameras/src/script.js
@@ -1,34 +1,37 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+console.log(OrbitControls);
+
 /**
  * The ArrayCamera is used to render your scene multiple times by using multiple cameras.
  * Each camera will render a specific area of the canvas.
  * You can imagine this looking like old school console multiplayer games where we had to share a split-screen.
- */
+*/
 
 /**
  * StereoCamera
  * The StereoCamera is used to render the scene through two cameras that mimic the eyes in order to create
  * what we call a parallax effect that will lure your brain into thinking that there is depth.
  * You must have the adequate equipment like a VR headset or red and blue glasses to see the result.
- */
+*/
 
 /**
  * CubeCamera
  * The CubeCamera is used to get a render facing each direction (forward, backward, leftward, rightward, upward, and downward) to create a render of the surrounding.
  * You can use it to create an environment map for reflection or a shadow map. We'll talk about those later.
- */
+*/
 
 /**
  * OrthographicCamera
  * The OrthographicCamera is used to create orthographic renders of your scene without perspective.
  * It's useful if you make an RTS game like Age of Empire. Elements will have the same size on the screen regardless of their distance from the camera.
- */
+*/
 
 /**
  * PerspectiveCamera
  * The PerspectiveCamera is the one we already used and simulated a real-life camera with perspective.
  * We are going to focus on the OrthographicCamera and the PerspectiveCamera.
- */
+*/
 
 // Cursor
 const cursor = {
@@ -58,43 +61,68 @@ const scene = new THREE.Scene()
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(mesh)
+    )
+    scene.add(mesh)
+    
+    // Camera
+    // const camera = new THREE.PerspectiveCamera(Vertical vision angle in degrees, sizes.width / sizes.height)
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+    // Ortho Camera
+    // const aspectRatio = sizes.width / sizes.height
+    // const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
+    
+    //camera.position.x = 2
+    //camera.position.y = 2
+    camera.position.z = 4
+    camera.lookAt(mesh.position)
+    scene.add(camera)
+    
+    // Controls
+    const controls = new OrbitControls(camera, canvas)
+    // The damping will smooth the animation by adding some kind of acceleration and friction formulas.
+    controls.enableDamping = true
+    // In order to work properly, the controls also needs to be updated on each frame by calling controls.update(). You can do that on the tick function:
 
-// Camera
-// const camera = new THREE.PerspectiveCamera(Vertical vision angle in degrees, sizes.width / sizes.height)
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-// Ortho Camera
-// const aspectRatio = sizes.width / sizes.height
-// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
 
-//camera.position.x = 2
-//camera.position.y = 2
-camera.position.z = 4
-camera.lookAt(mesh.position)
-scene.add(camera)
 
-// Built-in controls
-/**
- * If you type "controls" in the Three.js documentation, you'll see that there are a lot of pre-made controls.
- * 
- * DeviceOrientationControls
- * DeviceOrientationControls will automatically retrieve the device orientation if your device, OS, and browser allow it and rotate the camera accordingly.
- * You can use it to create immersive universes or VR experiences if you have the right equipment.
- * 
- * FlyControls
- * FirstPersonControls is just like FlyControls, but with a fixed up axis. You can see that like a flying bird view where the bird cannot do a barrel roll.
- * While the FirstPersonControls contains "FirstPerson," it doesn't work like in FPS games.
- * 
- * PointerLockControls
- * PointerLockControls uses the pointer lock JavaScript API. This API hides the cursor, keeps it centered, and keeps sending the movements in the
- * mousemove event callback. With this API, you can create FPS games right inside the browser. While this class sounds very promising if you want
- * to create that kind of interaction, it'll only handle the camera rotation when the pointer is locked. You'll have to handle the camera position and
- * game physics by yourself.
- * 
+    //By default, the camera is looking at the center of the scene. We can change that with the target property.
+   // controls.target.y = 4;
+   // controls.update()
+
+
+    
+    // Built-in controls
+    /**
+     * If you type "controls" in the Three.js documentation, you'll see that there are a lot of pre-made controls.
+     * 
+     * DeviceOrientationControls
+     * DeviceOrientationControls will automatically retrieve the device orientation if your device, OS, and browser allow it and rotate the camera accordingly.
+     * You can use it to create immersive universes or VR experiences if you have the right equipment.
+     * 
+     * FlyControls
+     * FirstPersonControls is just like FlyControls, but with a fixed up axis. You can see that like a flying bird view where the bird cannot do a barrel roll.
+     * While the FirstPersonControls contains "FirstPerson," it doesn't work like in FPS games.
+     * 
+     * PointerLockControls
+     * PointerLockControls uses the pointer lock JavaScript API. This API hides the cursor, keeps it centered, and keeps sending the movements in the
+     * mousemove event callback. With this API, you can create FPS games right inside the browser. While this class sounds very promising if you want
+     * to create that kind of interaction, it'll only handle the camera rotation when the pointer is locked. You'll have to handle the camera position and
+     * game physics by yourself.
+     * 
  * OrbitControls
  * OrbitControls is very similar to the controls we made in the previous lesson. You can rotate around a point with the left mouse, translate laterally
  * using the right mouse, and zoom in or out using the wheel.
+ * 
+ * TrackballControls
+ * TrackballControls is just like OrbitControls but there are no limits in terms of vertical angle. You can keep rotating and do spins with the camera
+ * even if the scene gets upside down.
+ * 
+ * TransformControls
+ * TransformControls has nothing to do with the camera. You can use it to add a gizmo to an object to move that object. its like an editor
+ * 
+ * DragControls
+ * Just like the TransformControls, DragControls has nothing to do with the camera. You can use it to move objects on a plane facing the camera by
+ * drag and dropping them.
  */
 
 
@@ -118,12 +146,15 @@ const tick = () =>
     // Update objects
    // mesh.rotation.y = elapsedTime;
    
-    // Update camera
+    /* Update camera
     camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2
     camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2
     camera.position.y = cursor.y * 3
-    camera.lookAt(mesh.position)
+    camera.lookAt(mesh.position) */
     
+    // In order to work properly, the Damping controls also needs to be updated on each frame by calling controls.update()
+    controls.update()
+
     camera.lookAt(mesh.position)
     // Render
     renderer.render(scene, camera)

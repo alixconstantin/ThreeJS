5b647062397b07cba003d03e200e6cd689bf5dfe
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-02-20 22:08:15 +0100

    Triangles

--- CHAP01-BASIC/09-geometries/src/script.js
+++ CHAP01-BASIC/09-geometries/src/script.js
@@ -17,14 +17,6 @@ const scene = new THREE.Scene()
 
 // Object
 //const geometry = new THREE.BoxGeometry(1, 1, 1, 2,2,  2)
-const geometry = new THREE.SphereGeometry(1, 32, 32)
-const material = new THREE.MeshBasicMaterial({
-     color: 0xff0000,
-     //wireframe : The wireframe will show the lines that delimit each triangle
-     wireframe: true
-})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 /**
  * The BoxGeometry has 6 parameters:
  * width: The size on the x axis
@@ -35,7 +27,87 @@ scene.add(mesh)
  * depthSegments: How many subdivisions in the z axis
  * The more subdivisions we add, the less we can distinguish the faces. But keep in mind that too many vertices and faces will affect performances.
  */
+const geometry = new THREE.SphereGeometry(1, 32, 32)
+const material = new THREE.MeshBasicMaterial({
+     color: 0xff0000,
+     //wireframe : The wireframe will show the lines that delimit each triangle
+     wireframe: true
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
+// Creating own buffer geometryxz
+// Create an empty BufferGeometry
+const geometryy = new THREE.BufferGeometry()
+// To add vertices to a BufferGeometry you must start with a Float32Array.
+// its native JS, only store floats, fixed length, easier to handle for the computer
+// To create a Float32Array, you can specify its length and then fill it later: 
+const positionsArray = new Float32Array(9)
+
+// First vertice
+positionsArray[0] = 0 // x
+positionsArray[1] = 0 // y
+positionsArray[2] = 0 // z
+
+// Second vertice
+positionsArray[3] = 0 // x
+positionsArray[4] = 1 // etc.
+positionsArray[5] = 0
+
+// Third vertice
+positionsArray[6] = 1
+positionsArray[7] = 0
+positionsArray[8] = 0
+
+// Or you can pass an array:
+
+const positionsArray = new Float32Array([
+    0, 0, 0, // First vertex
+    0, 1, 0, // Second vertex
+    1, 0, 0  // Third vertex
+])
+// Before you can send that array to the BufferGeometry, you have to transform it into a BufferAttribute
+// The first parameter corresponds to your typed array and the second parameter corresponds to how much values make one vertex attribute.
+// To read this array, we have to go 3 by 3 because a vertex position is composed of 3 values (x, y and z):
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+// We can add this attribute to our BufferGeometry using the setAttribute(...) method.
+// The first parameter is the name of this attribute and the second parameter is the value:
+geometry.setAttribute('position', positionsAttribute)
+
+/**
+// Create an empty BufferGeometry
+const geometry = new THREE.BufferGeometry()
+
+// Create a Float32Array containing the vertices position (3 by 3)
+const positionsArray = new Float32Array([
+    0, 0, 0, // First vertex
+    0, 1, 0, // Second vertex
+    1, 0, 0  // Third vertex
+])
+
+// Create the attribute and name it 'position'
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+geometry.setAttribute('position', positionsAttribute)
+ */
+
+/**
+ * We can also create a bunch of random triangles:
+ * 
+ // Create an empty BufferGeometry
+ * const geometry = new THREE.BufferGeometry()
+ //  Create 50 triangles (450 values)
+ *  const count = 50
+ * const positionsArray = new Float32Array(count * 3 * 3)
+ * for(let i = 0; i < count * 3 * 3; i++)
+ * {
+ *      positionsArray[i] = (Math.random() - 0.5) * 4
+ * }
+ // Create the attribute and name it 'position'
+ * const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+ * geometry.setAttribute('position', positionsAttribute)
+ * 
+ // count * 3 * 3 because We need 50 triangles. Each triangle is composed of 3 vertices and each vertex is composed of 3 values (x, y, and z).
+ */
 
 // Sizes
 const sizes = {

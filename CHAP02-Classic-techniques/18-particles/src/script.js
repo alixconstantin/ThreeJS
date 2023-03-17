5401e223aad5ab394521f5b4b14570b4a4737574
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-03-17 23:54:26 +0100

    circlePartices

--- CHAP02-Classic-techniques/18-particles/src/script.js
+++ CHAP02-Classic-techniques/18-particles/src/script.js
@@ -19,6 +19,8 @@ const scene = new THREE.Scene()
  */
 const textureLoader = new THREE.TextureLoader()
 
+const particleTexture = textureLoader.load('/textures/particles/2.png')
+
 
 
 
@@ -27,6 +29,8 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * Particles
  */
+
+/**  simple sphere as particles
 // Geometry
 const particlesGeometry = new THREE.SphereGeometry(1, 32, 32)
 
@@ -41,10 +45,39 @@ particlesMaterial.sizeAttenuation = true
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
+*/
 
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 50000
 
+const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
 
+for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
+{
+    positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+}
 
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.01,
+    sizeAttenuation: true,
+})
+particlesMaterial.size = 0.02
+particlesMaterial.sizeAttenuation = true
+particlesMaterial.color = new THREE.Color('#ff88cc')
+
+// We can also use the map property to put a texture on those particles.
+// * particlesMaterial.map = particleTexture
+// We need to activate transparency with transparent and use the texture on the alphaMap property instead of the map:
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 
 

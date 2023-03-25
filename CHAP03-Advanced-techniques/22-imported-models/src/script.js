a1fe84dd8dc147e68688064c4b67c85ba0598515
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-03-25 11:11:45 +0100

    PopularTypeOfFormat

--- CHAP03-Advanced-techniques/22-imported-models/src/script.js
+++ CHAP03-Advanced-techniques/22-imported-models/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+// list of popular formats : OBJ, FBX, STL, PLY, COLLADA, 3DS, GLTF
+
 
 /**
  * Base

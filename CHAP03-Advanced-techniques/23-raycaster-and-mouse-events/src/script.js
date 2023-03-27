efdf8b61862bb7d9c70a64e259283ab74b298919
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-03-28 00:51:38 +0200

    rayCastMouse

--- CHAP03-Advanced-techniques/23-raycaster-and-mouse-events/src/script.js
+++ CHAP03-Advanced-techniques/23-raycaster-and-mouse-events/src/script.js
@@ -2,6 +2,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 
+// a Raycaster can cast (or shoot) a ray in a specific direction and test what objects intersect with it
+// ( Like detect if there a wall in front of a player, test if the laser gun hit something, test if something is currently under
+//  the mouse to simulate mouse events, show an alert message if the spaceship is heading toward a planet etc. )
+
 /**
  * Base
  */
@@ -35,6 +39,55 @@ const object3 = new THREE.Mesh(
 object3.position.x = 2
 
 scene.add(object1, object2, object3)
+
+
+
+/**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) =>
+{
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1
+
+    console.log(mouse)
+})
+
+
+const raycaster = new THREE.Raycaster()
+let currentIntersect = null
+/**
+ * Raycaster ( for raycaster line impact)
+ 
+const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+const rayDirection = new THREE.Vector3(10, 0, 0)
+rayDirection.normalize() // normalize on a Vector 3 will reduce his size ( to length 1 ) but keep the direction
+
+// The first parameter is the position and the second parameter is the direction.
+raycaster.set(rayOrigin, rayDirection)
+
+// intersectObject(...) will test one object, intersectObjects(...) will test an array of objects:
+const intersect = raycaster.intersectObject(object2)
+console.log(intersect)
+
+const intersects = raycaster.intersectObjects([object1, object2, object3])
+console.log(intersects)
+*/
+/**
+Each item of that returned array contains much useful information:
+
+ - distance: the distance between the origin of the ray and the collision point.
+ - face: what face of the geometry was hit by the ray.
+ - faceIndex: the index of that face.
+ - object: what object is concerned by the collision.
+ - point: a Vector3 of the exact position in 3D space of the collision.
+ - uv: the UV coordinates in that geometry.
+
+ */
+
+
 
 /**
  * Sizes
@@ -83,11 +136,60 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
+
+
+// If we want to test things while they are moving, we have to do the test on each frame.
+// Let's animate the spheres and turn them blue when the ray intersects with them.
 const clock = new THREE.Clock()
 
+let currentIntersect = null
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+
+    // Cast a ray
+    // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0)
+    // rayDirection.normalize()
+    
+    // raycaster.set(rayOrigin, rayDirection)
+    
+    // const objectsToTest = [object1, object2, object3]
+    // const intersects = raycaster.intersectObjects(objectsToTest)
+    // console.log(intersects)
+
+    // for(const object of objectsToTest)
+    // {
+    //     object.material.color.set('#ff0000')
+    // }
+
+    // for(const intersect of intersects)
+    // {
+    //     intersect.object.material.color.set('#0000ff')
+    // }
+
+    raycaster.setFromCamera(mouse, camera)
+    
+    const objectsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+    
+    for(const intersect of intersects)
+    {
+        intersect.object.material.color.set('#0000ff')
+    }
+
+    for(const object of objectsToTest)
+    {
+        if(!intersects.find(intersect => intersect.object === object))
+        {
+            object.material.color.set('#ff0000')
+        }
+    }
 
     // Update controls
     controls.update()
